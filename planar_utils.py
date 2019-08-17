d4ca9e4a553343f4cca6883ef6f2f1b787fa6657
Jacob okoro <jaycobokoro@gmail.com>
2019-08-17 15:51:02 +0100

    made minor changes
    Signed-off-by: Jacob okoro <jaycobokoro@gmail.com>

--- planar_utils.py
+++ planar_utils.py
@@ -65,11 +65,9 @@ def load_planar_dataset():
 
 def load_extra_datasets():
     N = 200
-    noisy_circles = sklearn.datasets.make_circles(
-        n_samples=N, factor=.5, noise=.3)
+    noisy_circles = sklearn.datasets.make_circles(n_samples=N, factor=.5, noise=.3)
     noisy_moons = sklearn.datasets.make_moons(n_samples=N, noise=.2)
-    blobs = sklearn.datasets.make_blobs(
-        n_samples=N, random_state=5, n_features=2, centers=6)
+    blobs = sklearn.datasets.make_blobs(n_samples=N, random_state=5, n_features=2, centers=6)
     gaussian_quantiles = sklearn.datasets.make_gaussian_quantiles(
         mean=None, cov=0.5, n_samples=N, n_features=2, n_classes=2, shuffle=True, random_state=None)
     no_structure = np.random.rand(N, 2), np.random.rand(N, 2)
